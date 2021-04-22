17ec5d12c8e5b38a7a01fa979d93b74c97ca0971
vincentDarcq <vincent.darcq@hotmail.fr>
2021-04-22 21:43:44 +0200

    fix

--- server/environment/production.js
+++ server/environment/production.js
@@ -2,6 +2,6 @@ module.exports = {
   dbUrl: "mongodb+srv://vincent:uTLhewpTC1@cluster0.ic0uz.mongodb.net/EventMap?retryWrites=true&w=majority",
   cert: '/etc/letsencrypt/live/eventmap.site/fullchain.pem',
   key: '/etc/letsencrypt/live/eventmap.site/privkey.pem',
-  portHttp: 80,
+  portHttp: 300,
   portHttps: 443
 }
