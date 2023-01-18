b2d534d8f693c4291f73779c2fbac47f8cbfdb8d
Vincent Darcq <vincentdarcq@MacBook-Air-de-Vincent.local>
2023-01-18 22:47:28 +0100

    certificate path changed

--- server/environment/production.js
+++ server/environment/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   dbUrl: "mongodb+srv://vincent:uTLhewpTC1@cluster0.ic0uz.mongodb.net/EventMap?retryWrites=true&w=majority",
-  cert: '/etc/letsencrypt/live/www.eventmap.fr/privkey.pem',
+  cert: '/etc/letsencrypt/live/www.eventmap.fr/fullchain.pem',
   key: '/etc/letsencrypt/live/www.eventmap.fr/privkey.pem',
   portHttp: 80,
   portHttps: 443
