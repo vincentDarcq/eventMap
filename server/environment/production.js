1963ce4177751a17a0c2b81d4320db56a0b7dccb
Vincent Darcq <vincentdarcq@MacBook-Air-de-Vincent.local>
2023-04-03 15:31:22 +0200

    change db url

--- server/environment/production.js
+++ server/environment/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbUrl: "mongodb://vincent:<password>@cluster0-shard-00-00.ic0uz.mongodb.net:27017,cluster0-shard-00-01.ic0uz.mongodb.net:27017,cluster0-shard-00-02.ic0uz.mongodb.net:27017/?ssl=true&replicaSet=atlas-vg7d0o-shard-0&authSource=admin&retryWrites=true&w=majority",
+  dbUrl: "mongodb+srv://vincent:uTLhewpTC1@cluster0.ic0uz.mongodb.net/?retryWrites=true&w=majority",
   cert: '/etc/letsencrypt/live/www.eventmap.fr/fullchain.pem',
   key: '/etc/letsencrypt/live/www.eventmap.fr/privkey.pem',
   portHttp: 80,
